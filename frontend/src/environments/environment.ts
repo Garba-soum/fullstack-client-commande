bb01a12c5fc9fc765ca130c756e84a674d3e8cf4
Soumana Ibrahim <ibrahim_soumana1@yahoo.com>
2026-03-05 09:24:12 +0100

    IP dans front angular

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
 
-    apiUrl: 'http://localhost:8080' // URL de base du backend Spring Boot
+    //apiUrl: 'http://localhost:8080' // URL de base du backend Spring Boot
+    apiUrl: 'http://15.237.159.190:8081'
+
 };
